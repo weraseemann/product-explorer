09a5e66c5ae0ddcaf299d707ee22aed162b0df44
weraseemann <wera.seemann@gmail.com>
2025-07-16 16:39:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Produkt-Explorer
+Eine Single-Page-App “Produkt-Explorer”
+
+## Inhalte
+  1. Statische Produktliste (20–30 Einträge)
+  2. Live-Filter:
+• Textsuche (mit Debounce ~300 ms)
+• Kategorien-Checkboxen mit “Alle”-Logik
+• Preisslider (min/max)
+  3. Produkt-Grid mit Bild, Name, Preis, Kategorie
+  4. Responsives und barrierearmes Design
+  5. Kurzes README mit Setup-Anleitung und Erläuterungen
+
+Technische Vorgaben: 
+  - Vanilla JS + CSS
+  - Grid/Flexbox  
+  - Frameworks fürs Grund-Styling
+ 
+
+## Projektstruktur
+
+
+## Start-Anleitung
+1. Repository klonen: git clone 
+2. Öffne index.html in einem Browser (z. B. per Doppelklick oder mit Live Server)
+3. Instalation: Es ist keine installation notwendig.
+
+## Design-Entscheidungen
+
+- Verwendung von :root-Variablen für Wiederverwendbarkeit und einfache Pflege mit dem Ziel Zentrale Verwaltung von Farben, Schriftarten und Größen – leicht anpassbar für Theme-Änderungen.
+- Für Filterbereich: Blauer Hintergrund und Schatten ergibt optisch hervorgehobenen Bereich. Benutzung der Flexbox-Struktur für responsives Layout (Ausrichtung über flex-direction).
+- Füe Checkboxen: Farbliche Hervorhebung mit accent-color (modern & barriereärmer). Große Checkboxenfläche für bessere UX.
+- Interakriven und modernen Slider mit zwei Inputfeldern und einem Balken. 
+- Responsives Layout mit Media Queries (Schmalere Darstellung unter 768px und 481px) für Produktdarstellung, Filterbereich: Texteingabe, Checkboxen, Slider für gute UX auf Handy & Tablet.
+- Karten- und Listenstruktur für Inhalte durch automatische Anpassung mit auto-fill (mobilfreundlich).Responsive Grid-Darstellung von Produktkarten.
+- Benutzung der Buttonsproperties für Produktkartendarstellung: große buttons, klare Farben, leicht klickbar, optisch erkennbar als Call-to -Action. Es ist möglich Modale einzubauen für mehr Information über ein Produkt. 
+
+#### Zusammenfassung 
+Saubere Designprinzipien durch konsistente Fardben und Typography, gute UX mit responsiven Elementen, klare Trennung von Struktur, Design und Logik. 
---
+++ js/products-data.js
@@ -0,0 +1,129 @@
+export const products = [
+    {
+    Bild: "https://placehold.co/150x150/090/FFFFFF/jpg",
+    Name: "Produkt 1",
+    Preis: 200000,
+    Kategorie: "Auto",
+    },
+    {
+    Bild: "https://placehold.co/150x150/090/FFFFFF/jpg",
+    Name: "Produkt 2",
+    Preis: 10000,
+    Kategorie: "Auto",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 3",
+    Preis: 100,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/090/FFFFFF/jpg",
+    Name: "Produkt 4",
+    Preis: 20000,
+    Kategorie: "Auto",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 5",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/090/FFFFFF/jpg",
+    Name: "Produkt 6",
+    Preis: 30000,
+    Kategorie: "Auto",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 7",
+    Preis: 300,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/090/FFFFFF/jpg",
+    Name: "Produkt 8",
+    Preis: 25000,
+    Kategorie: "Auto",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 9",
+    Preis: 250,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 10",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 11",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 12",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 13",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 14",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/090/FFFFFF/jpg",
+    Name: "Produkt 15",
+    Preis: 100000,
+    Kategorie: "Auto",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 16",
+    Preis: 300,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 17",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 18",
+    Preis: 200,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/090/FFFFFF/jpg",
+    Name: "Produkt 19",
+    Preis: 25000,
+    Kategorie: "Auto",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 20",
+    Preis: 199,
+    Kategorie: "Autozubehör",
+    },
+    {
+    Bild: "https://placehold.co/150x150/009/FFFFFF/jpg",
+    Name: "Produkt 21",
+    Preis: 99,
+    Kategorie: "Autozubehör",
+    }
+
+];
